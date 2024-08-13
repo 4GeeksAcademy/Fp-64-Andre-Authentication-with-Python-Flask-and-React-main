f3f1c10b9a01976b1a06409912bf6c2d6a5a6216
Andre Cortes <52327276+andrecortes1997@users.noreply.github.com>
2024-08-13 14:56:36 +0000

    Authentication_Flask

---
+++ src/front/js/layout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ScrollToTop from "./component/scrollToTop";
+import { BackendURL } from "./component/backendURL";
+
+import { Home } from "./pages/home";
+import { SignUp } from "./pages/SignUp.jsx";
+import { Login} from "./pages/Login.jsx"
+import { Private } from "./pages/Private.jsx";
+import { Main } from "./pages/Main.jsx";   
+import { Demo } from "./pages/demo";
+import { Single } from "./pages/single";
+import injectContext from "./store/appContext";
+
+
+//create your first component
+const Layout = () => {
+    //the basename is used when your project is published in a subdirectory and not in the root of the domain
+    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+    const basename = process.env.BASENAME || "";
+
+    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+
+    return (
+        <div>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Routes>
+                        <Route element={<Home />} path="/home" />
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Main />} path="/" /> 
+                        <Route element={<SignUp />} path="/signup" /> 
+                        <Route element={<Login />} path="/login" /> 
+                        <Route element={<Private />} path="/private" /> 
+                    </Routes>
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default injectContext(Layout);
